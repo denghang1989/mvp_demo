92c90f1ddf8d6b53752b00692a5078de029e6ec1
denghang <hang.deng@fgecctv.com>
2016-09-30 16:11:59 +0800

    接口调试

--- module/src/main/java/fgecctv/com/module/remote/BaseEntity.java
+++ module/src/main/java/fgecctv/com/module/remote/BaseEntity.java
@@ -1,7 +1,5 @@
 package fgecctv.com.module.remote;
 
-import android.util.Log;
-
 import rx.Observable;
 import rx.Subscriber;
 
@@ -9,14 +7,12 @@ import rx.Subscriber;
  * 2016/9/29 13
  */
 public abstract class BaseEntity<T> implements BaseFunc1<BaseResultEntity<T>, T> {
-    private static final String TAG = "BaseEntity";
     public abstract Observable getObservable(HttpService method);
 
     public abstract Subscriber getSubscriber();
 
     @Override
     public T call(BaseResultEntity<T> resultEntity) {
-        Log.d(TAG, "call: "+resultEntity.getCode());
         if (resultEntity.getCode() != 0) {
             throw new HttpTimeException(resultEntity.getMsg());
         }
